68da0c24f8de85a356827cd2907f96c6b1f1bd79
Bischi <Bischi@live.at>
2014-01-09 12:29:22 +0000

    loginWindow erstellt

---
+++ CustomerManager/loginWindow.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CustomerManager
+{
+	public partial class loginWindow : Gtk.Window
+	{
+		public loginWindow () : 
+				base(Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+		}
+	}
+}
+
