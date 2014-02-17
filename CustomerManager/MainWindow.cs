32f07612ceeb57fcc59a4998efde32df3c76de0e
Andreas <adnreas@bischoffritz.at>
2014-02-17 16:32:32 +0100

    Fail

--- CustomerManager/MainWindow.cs
+++ CustomerManager/MainWindow.cs
@@ -7,33 +7,6 @@ using CustomerManager;
 
 
 
-<<<<<<< HEAD
-public partial class MainWindow: Gtk.Window
-{
-	public MainWindow () : base (Gtk.WindowType.Toplevel)
-	{
-		Build ();
-		this.Title = "Customer Manager";
-	}
-
-	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
-	{
-		Application.Quit ();
-		a.RetVal = true;
-	}
-
-	protected void OnAddCustomerMBActivated (object sender, EventArgs e)
-	{
-		CustomerWindow cw = new CustomerWindow();
-		cw.ShowAll();
-	}
-	protected void OnAddCustomerTBActivated (object sender, EventArgs e)
-	{
-		CustomerWindow cw = new CustomerWindow();
-		cw.Show ();
-	}
-}
-=======
 public partial class MainWindow: Gtk.Window
 {
 	public MainWindow () : base (Gtk.WindowType.Toplevel)
@@ -64,4 +37,4 @@ public partial class MainWindow: Gtk.Window
 		cw.Show ();
 	}
 }
->>>>>>> 27a1df76239bb9f24f8c071ed251ab843957f8b7
+
